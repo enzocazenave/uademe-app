d17a3132a1e653d4d5ccb644e8c474614415e5a2
Enzo Cazenave <encaze24@hotmail.com>
2023-03-29 19:31:45 -0300

    Add boilerplate code

---
+++ UadeMeApp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    Text,
+    View,
+} from 'react-native';
+
+export const App = () => {
+    return (
+        <SafeAreaView>
+            <Text>Uade Me</Text>
+        </SafeAreaView>
+    )
+}
