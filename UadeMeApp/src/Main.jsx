3c0bfe2a19b1612148855a925357e13af7ddf4d6
Enzo Cazenave <encaze24@hotmail.com>
2023-04-05 19:41:11 -0300

    Add couplequestion

--- UadeMeApp/src/Main.jsx
+++ UadeMeApp/src/Main.jsx
@@ -2,13 +2,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { Navigation } from './navigation/Navigation';
 import { AuthProvider } from './context/AuthContext';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 export const Main = () => {
     return (
-        <NavigationContainer>
-            <AuthProvider>
-                <Navigation />
-            </AuthProvider>
-        </NavigationContainer>
+        <TouchableWithoutFeedback
+            onPress={ Keyboard.dismiss }
+        >
+            <NavigationContainer>
+                <AuthProvider>
+                    <Navigation />
+                </AuthProvider>
+            </NavigationContainer>
+        </TouchableWithoutFeedback>
     );
 }
